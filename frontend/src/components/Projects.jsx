3b98e65985cfef15d6523f3629b5744ce9733fab
Daniel Barra <danielbarra@MacBook-Pro-de-Daniel.local>
2025-10-27 19:39:39 -0300

    agregar nueva imagen proyecto

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -16,6 +16,11 @@ function Projects(){
          imagen : "/images/Proyecto1.jpeg",
          link: "https://www.pselizabethbarra.cl/"
         },
+        {titulo: "Sitio Psicologia online",
+            descripcion: "Sitio web con reservas y envio automatico de correos.", 
+            imagen : "/images/Proyecto1.jpeg",
+            link: "https://www.pselizabethbarra.cl/"
+        }
         
     ];
 
